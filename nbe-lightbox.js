948ca55822916c1b033b4c3bcdc245e33958a0a0
noshad <nbe0516@gmail.com>
2020-12-11 01:15:06 +0330

    first commit

--- nbe-lightbox.js
+++ nbe-lightbox.js
@@ -1,4 +1,23 @@
 class NbeLightbox{
-	
+	constructor(element,options){
+        this.el = element;
+        this.initiate();
+    }
+
+    initiate(){
+        let neededElements = `
+            <div class="nbe-lb-container">
+                <div class="owl-carousel">
+                    
+                </div>
+            </div>
+        `;
+        $("body").append(neededElements);
+    }
 	
 }
+
+$(document).ready(function(){
+    const someName = new NbeLightbox('.some-class')
+
+})
