df62e89146d4d79cd18d7a556c7e1f383829a349
noshad <nbe0516@gmail.com>
2021-01-21 16:45:51 +0330

    enable two light box in same page

--- nbe-lightbox.js
+++ nbe-lightbox.js
@@ -1,8 +1,9 @@
 class NbeLightbox{
-	constructor(element,options){
+    constructor(element,options){
         if(!options){
             var options = {};
         }
+        this.randText = "nbe"+ Math.floor(Math.random() * 1000 );
         this.el = element;
         this.bgColor = options.bgColor?options.bgColor:"black";//background color of lightbox
         this.captionsClass = options.captionsClass?options.captionsClass:".there-is-no-caption-for-images";//camption must be next to the img
@@ -32,7 +33,16 @@ class NbeLightbox{
             </g>
             </svg>
         `;
-        this.closeBtn = options.closeBtn?options.closeBtn:`<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" version="1.1" x="0" y="0" viewBox="0 0 329.26933 329" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><g><path xmlns="http://www.w3.org/2000/svg" d="m194.800781 164.769531 128.210938-128.214843c8.34375-8.339844 8.34375-21.824219 0-30.164063-8.339844-8.339844-21.824219-8.339844-30.164063 0l-128.214844 128.214844-128.210937-128.214844c-8.34375-8.339844-21.824219-8.339844-30.164063 0-8.34375 8.339844-8.34375 21.824219 0 30.164063l128.210938 128.214843-128.210938 128.214844c-8.34375 8.339844-8.34375 21.824219 0 30.164063 4.15625 4.160156 9.621094 6.25 15.082032 6.25 5.460937 0 10.921875-2.089844 15.082031-6.25l128.210937-128.214844 128.214844 128.214844c4.160156 4.160156 9.621094 6.25 15.082032 6.25 5.460937 0 10.921874-2.089844 15.082031-6.25 8.34375-8.339844 8.34375-21.824219 0-30.164063zm0 0" fill="#ffffff" data-original="#000000" style="" class=""/></g></svg>`;
+        this.closeBtn = options.closeBtn?options.closeBtn:`
+        <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 45 45">
+  <g id="Group_3524" data-name="Group 3524" transform="translate(-332.75 -42)">
+    <circle id="Ellipse_786" data-name="Ellipse 786" cx="22.5" cy="22.5" r="22.5" transform="translate(332.75 42)" fill="#fff"/>
+    <line id="Li22ne_4114" data-name="Line 4114" x2="17.5" transform="translate(348.813 58.313) rotate(45)" fill="none" stroke="#3596b5" stroke-linecap="round" stroke-width="2"/>
+    <line id="Li22ne_4115" data-name="Line 4115" x2="17.5" transform="translate(361.187 58.313) rotate(135)" fill="none" stroke="#3596b5" stroke-linecap="round" stroke-width="2"/>
+  </g>
+</svg>
+
+        `;
         this.initiate();
     }
 
@@ -40,13 +50,15 @@ class NbeLightbox{
         var self = this;
         var captionsClass = this.captionsClass;
         let el = this.el;
+        var randText = self.randText;
         let neededElements = `
-            <div class="nbe-lb-container" style="background-color:${this.bgColor}">
+            <div class="${randText} nbe-lb-container" style="background-color:${this.bgColor}">
                 <p class="lb-caption-title">test caption</p>
-                <div id="nbe-lb-close">${self.closeBtn}</div>
+                <div class="nbe-lb-close">${self.closeBtn}</div>
                 <div class="owl-carousel owl-theme"></div>
             </div>
         `;
+
         $(el+" a").click(function(e){
             e.preventDefault();
         });
@@ -55,11 +67,11 @@ class NbeLightbox{
         $(el+" img").each(function(){
             $(this).attr('data-owlItem', i);
             var captionText = $(this).next(captionsClass).text();
-            $(this).clone().appendTo(".nbe-lb-container .owl-carousel").wrap( "<div class='item' data-caption='"+ captionText +"'></div>" );
+            $(this).clone().appendTo("."+ randText +".nbe-lb-container .owl-carousel").wrap( "<div class='item' data-caption='"+ captionText +"'></div>" );
             i++;
         });
-        var nbeLBOwl = $('.nbe-lb-container .owl-carousel').owlCarousel({
-            loop:true,
+        var nbeLBOwl = $("."+ randText +".nbe-lb-container .owl-carousel").owlCarousel({
+            loop:false,
             margin:10,
             dots:false,
             nav:true,
@@ -70,21 +82,21 @@ class NbeLightbox{
                 },
             }
         })
-        $('.nbe-lb-container .owl-carousel .owl-nav').wrap(`<div class="nbe-lb-contaienr"></div>`);
+        $('.nbe-lb-container .owl-carousel .owl-nav').wrap(`<div class="${randText} nbe-lb-contaienr"></div>`);
         nbeLBOwl.on('changed.owl.carousel', function(event) {
             setTimeout(function(){
                 let caption = $(".nbe-lb-container .owl-item.active .item").data("caption");
                 $(".lb-caption-title").text(caption);
             },100)
         });
-        $("#nbe-lb-close").click(function(){
-            $(".nbe-lb-container").fadeOut(1000)
+        $(".nbe-lb-close").click(function(){
+            $("."+ randText +".nbe-lb-container").fadeOut(1000)
         });
         $(el+" img").click(function(e){
             e.preventDefault();
-           var owlItem = $(this).data("owlitem");
+            var owlItem = $(this).data("owlitem");
             nbeLBOwl.trigger('to.owl.carousel', [owlItem, 0]);
-            $(".nbe-lb-container").fadeIn();
+            $("."+ randText +".nbe-lb-container").fadeIn();
         });
     }
 }
